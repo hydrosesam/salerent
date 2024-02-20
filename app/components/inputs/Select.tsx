17e019cc5305bd10af950d46e9995ad3dd8888ec
hydrosesam <71249432+hydrosesam@users.noreply.github.com>
2024-02-20 15:26:17 +0530

    Update Select.tsx

--- app/components/inputs/Select.tsx
+++ app/components/inputs/Select.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Select from "react-select";
 
 interface SelectProps {
   id: string;
@@ -7,57 +8,31 @@ interface SelectProps {
   onChange: (value: string) => void;
 }
 
-const Select: React.FC<SelectProps> = ({ id, options, value, onChange }) => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [selectedItem, setSelectedItem] = useState<string | null>(null);
-  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+const Select1: React.FC<SelectProps> = ({ id, options, value, onChange }) => {
+  const [selectedOption, setSelectedOption] = useState<{
+    value: string;
+    label: string;
+  } | null>(null);
 
-  const filteredOptions = options.filter((option) =>
-    option.label.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-    setIsDropdownOpen(true);
-  };
-
-  const handleOptionSelect = (selectedValue: string) => {
-    const selectedOption = options.find(
-      (option) => option.value === selectedValue
-    );
+  const handleChange = (
+    selectedOption: { value: string; label: string } | null
+  ) => {
+    setSelectedOption(selectedOption);
     if (selectedOption) {
-      setSelectedItem(selectedOption.label);
-      setSearchTerm("");
-      setIsDropdownOpen(false);
-      onChange(selectedValue);
+      onChange(selectedOption.value);
     }
   };
 
   return (
     <div className="flex flex-col relative">
-      <input
-        type="text"
+      <Select
         placeholder="Search..."
-        value={selectedItem || searchTerm}
-        onChange={handleInputChange}
-        onFocus={() => setIsDropdownOpen(true)}
-        className="border-[2px] p-3 pt-5 pb-5 relative appearance-none text-gray-500"
+        value={selectedOption} // Set the selected value
+        options={options} // Set the options
+        onChange={handleChange} // Handle selection change
       />
-      {isDropdownOpen && (
-        <div className="absolute top-full left-0 mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10">
-          {filteredOptions.map((item) => (
-            <div
-              key={item.value}
-              className="p-2 cursor-pointer hover:bg-gray-100"
-              onClick={() => handleOptionSelect(item.value)}
-            >
-              {item.label}
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 };
 
-export default Select;
+export default Select1;
